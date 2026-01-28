6757970ea5b41f335162594df0fbe07ef43b0f12
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-28 09:21:29 +0530

    java basics

--- JavaBasics6.java
+++ JavaBasics6.java
@@ -170,5 +170,24 @@ public class JavaBasics6 {
     // }
 
 
+    // //binary to decimal conversion
+    // public static void binToDec (int binNum) {
+    //     int myNum = binNum;
+    //     int pow = 0;
+    //     int dec = 0;
+
+    //     while (binNum > 0) {
+    //         int lastDigit = binNum % 10;
+    //         dec = dec + (lastDigit * (int)Math.pow(2, pow));
+
+    //         pow ++;
+    //         binNum = binNum / 10;
+    //     }
+    //     System.out.println("Decimal of " + myNum +" = " + dec);
+    // }
+    // public static void main (String args[]) {
+    //     binToDec(101);
+    // }
+
 
 
