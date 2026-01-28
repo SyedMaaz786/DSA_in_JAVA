086e2ac8c0c596b56f2271948a2b5e11238bf3fc
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-28 21:39:00 +0530

    java basics

--- JavaBasics7.java
+++ JavaBasics7.java
@@ -95,4 +95,44 @@ public class JavaBasics7 {
 // }
 
 
+// //Butterfly pattern
+// public static void Butterfly (int n) {
+//     //1st half
+//     for (int i=1; i<=n; i++) {  //Here i is runned from 1 to n, i for printing how many number of rows
+//         //stars(i)
+//         for (int j=1; j<=i; j++) {  //j for printing how many number of cols
+//             System.out.print("*");
+//         }
+//         //spaces(2*n-i)
+//         for (int j=1; j<=2*(n-i); j++) {
+//             System.out.print(" ");
+//         }
+//         //stars(i)
+//         for (int j=1; j<=i; j++) {
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+//     //2nd half
+//     for (int i=n; i>=1; i--) {  //Here i is runned from n to 1
+//         //stars(i)
+//         for (int j=1; j<=i; j++) {  //j for printing how many number of cols
+//             System.out.print("*");
+//         }
+//         //spaces(2*n-i)
+//         for (int j=1; j<=2*(n-i); j++) {
+//             System.out.print(" ");
+//         }
+//         //stars(i)
+//         for (int j=1; j<=i; j++) {
+//             System.out.print("*");
+//         }
+//         System.out.println();
+//     }
+// }    
+// public static void main (String args[]) {
+//     Butterfly(4);
+// }
+
+
 }
