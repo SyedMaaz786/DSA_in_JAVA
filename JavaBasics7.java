dc80bfbf12d080b20a6a5f9b4d925e1cce2e1ecc
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-28 12:01:49 +0530

    java basics

---
+++ JavaBasics7.java
@@ -0,0 +1,20 @@
+public class JavaBasics7 {
+//     public static void hollow_rectangle (int totRows, int totCols) {
+//         for (int i=1; i<=totRows; i++) { // outer loop for printing lines
+//             for (int j=1; j<=totCols; j++) {  //inner loop for printing *
+//                 if (i == 1 || i == totRows  || j == 1 || j == totCols) {
+//                     System.out.print("*");
+//                 }
+//                 else {
+//                     System.out.print(" ");
+//                 } 
+//             }
+//             System.out.println();  //after traversing one complete line to come to the next line
+//         }
+//     }
+//     public static void main (String args[]) {
+//         hollow_rectangle(9,8);
+    // }
+
+    
+}
