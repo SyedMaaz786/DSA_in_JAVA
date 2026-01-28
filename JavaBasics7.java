12a85b2b027d97f9c071b3065b8f670514fc7e8a
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-28 12:46:32 +0530

    java basics

--- JavaBasics7.java
+++ JavaBasics7.java
@@ -1,4 +1,6 @@
 public class JavaBasics7 {
+// // Advanced pattern (using matrices) 
+// // Hollow_rectangle   
 //     public static void hollow_rectangle (int totRows, int totCols) {
 //         for (int i=1; i<=totRows; i++) { // outer loop for printing lines
 //             for (int j=1; j<=totCols; j++) {  //inner loop for printing *
@@ -14,7 +16,27 @@ public class JavaBasics7 {
 //     }
 //     public static void main (String args[]) {
 //         hollow_rectangle(9,8);
-    // }
+//     }
 
-    
+
+// // Half-Pyramid
+// public static void half_pyr (int totRows, int totCols) { //dry run you will get it
+//     for (int i=1; i<=totRows; i++) {     //outer loop i for lines
+//         for (int j=1; j<=totCols; j++) { // inner loop j for stars
+//             if (j > totCols - i) {       //and keep staring this if condition you will get it 
+//                 System.out.print("*");    
+//             }
+//             else {
+//                 System.out.print(" ");
+//             }
+//         }
+//         System.out.println();
+//     }
+// }
+// public static void main (String args[]) {
+//     half_pyr(4, 4);
+// }
+
+
+
 }
