0a6ccd7cc814a956b7bdfedd5c86b40b085133cf
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-29 17:51:08 +0530

    Arrays

--- b_JavaArrays01.java
+++ b_JavaArrays01.java
@@ -79,7 +79,22 @@ public class b_JavaArrays01 {
 //     else {
 //         System.out.print(arr[index]);
 //     }
-    
+// }
+
+
+// //largest number in an arr
+// public static int largestNumb (int arr[]) {
+//         int largest = Integer.MIN_VALUE; //-infinity (Specifies the smallest value)
+//         for (int i=0; i<arr.length; i++) {  //to find smallest just give +infinity ie max_value
+//             if (arr[i] > largest) { //we are comparing for each and every element in our arr with the smallest number we defined above ie -∞
+//                 largest = arr[i];
+//             }
+//         }
+//         return largest;
+// }
+// public static void main(String args[]) {
+//     int arr[] = {1,2,6,3,5};
+//     System.out.println("Largest value = " + largestNumb(arr));
 // }
 
 
