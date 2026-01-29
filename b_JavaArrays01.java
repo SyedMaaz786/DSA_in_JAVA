c0df8f3f78b94a47daa805f87fd901d989332159
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-29 12:42:56 +0530

    Java Basics

---
+++ b_JavaArrays01.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class b_JavaArrays01 {
+    public static void main (String args[]) {
+        // int marks[] = new int[50]; //explicitly declaring size ie 50
+        // int numbers[] = {1,2,3};  //Here we are directly specifying the values java will automatically detect the size
+        // String myName[] = {"Syed", "Maaz"};
+        // System.out.println(marks.length);
+        // System.out.println(Arrays.toString(numbers)); //we are using type conversion
+        // System.out.println(Arrays.toString(myName));
+
+
+        int marks[] = new int[100];  //Here we have created a array of length 100
+        Scanner sc = new Scanner(System.in);
+        //using indexing we are taking input on that particular arr index
+        marks[0] = sc.nextInt();  //phy
+        marks[1] = sc.nextInt();  //chem
+        marks[2] = sc.nextInt();  //math
+        System.out.println("Phy = " + marks[0]);
+        System.out.println("Chem = " + marks[1]);
+        System.out.println("Math = " + marks[2]);
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;   //taking avg
+        System.out.println("Percentage = " + percentage);
+
+    } 
+}
