a35098fd3c0928f1d6c605a013b9f67b120cabe9
Syed Maaz <syedmaaz78622@gmail.com>
2026-01-30 08:48:04 +0530

    Arrays

--- b_JavaArrays01.java
+++ b_JavaArrays01.java
@@ -150,4 +150,25 @@ public class b_JavaArrays01 {
 // }
 
 
+
+// // //Reverse an array
+// public static void reverseArr (int numbers[]) {
+//     int start = 0, end = numbers.length-1;
+//     while (start < end) {
+//         int temp = numbers[start];
+//         numbers[start] = numbers[end];
+//         numbers[end] = temp;
+//         start ++;  //we will swap from start and end value so start will increment and go to next from last end decrement and come one value reverse
+//         end --;
+//     }
+// }
+// public static void main(String args[]) {
+//     int numbers[] = {1,2,3,4,5,6,7,8,9};
+//     reverseArr(numbers);
+//     for (int i=0; i<numbers.length; i++) {
+//         System.out.print(numbers[i]);
+//     }
+//     System.out.println();
+// }
+
 }
