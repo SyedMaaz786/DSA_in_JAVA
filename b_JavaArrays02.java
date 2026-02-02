749d0495d504f6c6d07e56255239235d316cd8ea
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-02 14:26:21 +0530

    Arrays

--- b_JavaArrays02.java
+++ b_JavaArrays02.java
@@ -1,6 +1,6 @@
 public class b_JavaArrays02 {
     
-// // subarray sum with max sum (Brute Force)
+// // subarray sum with max sum (Brute Force) - time complexity On3
 // public static void maxSubarraySum(int arr[]) {
 //     int currSum = 0;
 //     int maxSum = Integer.MIN_VALUE;
@@ -22,6 +22,34 @@ public class b_JavaArrays02 {
 //     int arr[] = {1,2,3,4};
 //     maxSubarraySum(arr);
 // }
+
+
+// // //max subarray sum (prefix sum) - time complexity On2
+// public static void maxSubarraySum(int arr[]) {
+//     int currSum = 0;
+//     int maxSum = Integer.MIN_VALUE;
+//     int prefix[] = new int[arr.length];
+
+//     //calc prefix array
+//     prefix[0] = arr[0];
+//     for (int i=1; i<prefix.length; i++) {
+//         prefix[i] = prefix[i-1] + arr[i];  //Here we are defining the prefix[0] = arr[0] in the start itself because if we start the loop from i=0, then we will get -1 as the index value which invalid
+//     }                                      //logic to sum the values and store it in the prefix arr 
+//     for (int i=0; i<arr.length; i++) {
+//         for (int j=i; j<arr.length; j++) {
+//             currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+//             if (currSum > maxSum) {
+//                 maxSum = currSum;
+//             }
+//         }
+//     }
+//     System.out.println("max sum: " + maxSum);
+// }
+// public static void main (String args[]) {
+//     int arr[] = {1,2,3,4};
+//     maxSubarraySum(arr);
+// }
+
 
 
 
