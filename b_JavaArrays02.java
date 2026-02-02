c0cd66f6ff83c33fa384c87372d909f9dade4762
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-02 13:26:51 +0530

    Arrays

--- b_JavaArrays02.java
+++ b_JavaArrays02.java
@@ -1,3 +1,28 @@
 public class b_JavaArrays02 {
     
+// // subarray sum with max sum (Brute Force)
+// public static void maxSubarraySum(int arr[]) {
+//     int currSum = 0;
+//     int maxSum = Integer.MIN_VALUE;
+//     for (int i=0; i<arr.length; i++) {
+//         for (int j=i; j<arr.length; j++) {
+//             currSum = 0;
+//             for (int k=i; k<=j; k++) {
+//                 currSum = currSum + arr[k];
+//             }
+//             System.out.println(currSum);
+//             if (currSum > maxSum) {
+//                 maxSum = currSum;
+//             }
+//         }
+//     }
+//     System.out.println("max sum = " + maxSum);
+// }
+// public static void main (String args[]) {
+//     int arr[] = {1,2,3,4};
+//     maxSubarraySum(arr);
+// }
+
+
+
 }
