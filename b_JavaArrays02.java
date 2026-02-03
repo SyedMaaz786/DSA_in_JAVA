e149273bc0107becfb26461b24084b89398f5bf4
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-03 09:49:13 +0530

    Arrays

--- b_JavaArrays02.java
+++ b_JavaArrays02.java
@@ -72,6 +72,34 @@ public class b_JavaArrays02 {
 // }
 
 
+// //Trapped water
+// public static int trappedRainWater (int height[]) {
+//     //calc left max height using auxillary arr
+//     int leftMax[] = new int[height.length];
+//     leftMax[0] = height[0];
+//     for (int i=1; i<height.length; i++) {
+//         leftMax[i] = Math.max(height[i], leftMax[i-1]);
+//     }
+//     //calc right max height using auxillary arr
+//     int rightMax[] = new int[height.length];
+//     rightMax[height.length-1] = height[height.length-1];
+//     for (int i=height.length-2; i>=0; i--) {
+//         rightMax[i] = Math.max(height[i], rightMax[i+1]);
+//     }
+//     //loop
+//     int trappedWater = 0;
+//     for (int i=0; i<height.length; i++) {
+//         //waterlevel = min(left max height, right max height)
+//         int waterLevel = Math.min(leftMax[i], rightMax[i]);
+//         //trapped water = waterlevel - height[i]
+//         trappedWater = trappedWater + waterLevel - height[i];
+//     }
+//     return trappedWater;
 
+// }
+// public static void main (String args[]) {
+//     int height[] = {4,2,0,6,3,2,5};
+//     System.err.println(trappedRainWater(height));
+// }
 
 }
