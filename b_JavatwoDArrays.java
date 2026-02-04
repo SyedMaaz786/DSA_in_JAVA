6f767ec4cd6710c6f71b0844235b21bffcc74c27
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-04 10:37:39 +0530

    Arrays

--- b_JavatwoDArrays.java
+++ b_JavatwoDArrays.java
@@ -1,3 +1,37 @@
+import java.util.*;
 public class b_JavatwoDArrays {
-    
+    // // fnx for finding the pos of the element
+    // public static boolean search(int twoD[][], int key) {  
+    //     for (int i=0; i<twoD.length; i++) {
+    //         for (int j=0; j<twoD[0].length; j++) {
+    //             if (twoD[i][j] == key) {
+    //                 System.out.print("Key found at " + i + "," + j);
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     System.out.print("Key NOT found");
+    //     return false;
+    // }
+    // // fnx for twoDArr
+    // public static void main(String args[]) {
+    //     int twoD[][] = new int [3][3];
+    //     int n = twoD.length, m = twoD[0].length;
+    //     Scanner sc = new Scanner(System.in);
+    //     for (int i=0; i<n; i++) {   //for rows
+    //         for (int j=0; j<m; j++) {  //for cols
+    //             twoD[i][j] = sc.nextInt();  //taking i/p
+    //         }
+    //     }
+    //     sc.close();
+    //     //output
+    //     for (int i=0; i<n; i++) {
+    //         for (int j=0; j<m; j++) {
+    //             System.out.print(twoD[i][j]);
+    //         }
+    //         System.out.println();
+    //     }
+    //     search(twoD, 4);
+    // }
+
 }
