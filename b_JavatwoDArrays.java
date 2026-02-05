110d023acb2e31b76fcf9468604e6c98458d4f02
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-05 09:38:42 +0530

    2D Arrays

--- b_JavatwoDArrays.java
+++ b_JavatwoDArrays.java
@@ -1,16 +1,33 @@
 import java.util.*;
 public class b_JavatwoDArrays {
+    // //fnx to find min and max number in the arr
+    // public static void findMinMax(int twoD[][]) {
+    //     int min = twoD[0][0];
+    //     int max = twoD[0][0];
+    //     for (int i=0; i<twoD.length; i++) {
+    //         for (int j=0; j<twoD[0].length; j++) {
+    //             if (twoD[i][j] < min) {
+    //                 min = twoD[i][j];
+    //             }
+    //             if (twoD[i][j] > max) {
+    //                 max = twoD[i][j];
+    //             }
+    //         }
+    //     }
+    //     System.out.println("Min is:" + min);
+    //     System.out.println("Max is:" + max);
+    // }
     // // fnx for finding the pos of the element
     // public static boolean search(int twoD[][], int key) {  
     //     for (int i=0; i<twoD.length; i++) {
     //         for (int j=0; j<twoD[0].length; j++) {
     //             if (twoD[i][j] == key) {
-    //                 System.out.print("Key found at " + i + "," + j);
+    //                 System.out.println("Key found at " + i + "," + j);
     //                 return true;
     //             }
     //         }
     //     }
-    //     System.out.print("Key NOT found");
+    //     System.out.println("Key NOT found");
     //     return false;
     // }
     // // fnx for twoDArr.
@@ -32,6 +49,7 @@ public class b_JavatwoDArrays {
     //         System.out.println();
     //     }
     //     search(twoD, 4);
+    //     findMinMax(twoD);
     // }
 
 }
