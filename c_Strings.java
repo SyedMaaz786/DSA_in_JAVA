94690d13527765d99ab4b6a5b0c095eb7fab2d1d
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-05 20:29:37 +0530

    String

--- c_Strings.java
+++ c_Strings.java
@@ -130,6 +130,19 @@ public class c_Strings {
     // }
 
 
+    // //Largest String (lexicographical order)
+    // public static void main(String args[]) {
+    //     String str[] = {"apple", "mango", "banana", "carrot"};
+    //     String largest = str[0];
+    //     for (int i=1; i<str.length; i++) {
+    //         if (largest.compareTo(str[i])  < 0) {  // (<0 largest is smaller), (0 both strings are equal), (>0 largest is bigger)
+    //             largest = str[i];
+    //         }
+    //     }
+    //     System.out.println(largest);
+    // }
+
+
 
 
 }
