e0306f0dc8f2de21bb6e71a17d0bc8cd0f8f5520
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-05 18:57:53 +0530

    String

--- c_Strings.java
+++ c_Strings.java
@@ -1,3 +1,61 @@
+import java.util.*;
 public class c_Strings {
-    
+    // public static void main(String args[]) {
+    //     // char arr[] = {'a', 'b', 'c', 'd'};
+    //     // String str = "abcd";
+    //     // String str2 = new String("xyz");
+    //     // //Strings are IMMUTABLE
+    //     // Scanner sc = new Scanner(System.in);
+    //     // String name = sc.nextLine();
+    //     // System.out.println(name);
+    //     // System.out.println(name.length());
+
+    //     //concatenation
+    //     String firstName = "Syed";
+    //     String lastName = "Maaz";
+    //     String fullName = firstName + " " + lastName;
+    //     System.out.println(fullName);
+    // }
+
+
+    // //print string elements using charAt
+    // //print element at particular index
+    // public static void printelementsbyindex(String args[]) {
+    //     String str = "Syed Maaz";
+    //     System.out.println(str.charAt(5));
+    // }
+    // //loop to print all elements in the string 
+    // public static void printStringelements(String str) {
+    //     for (int i=0; i<str.length(); i++) {
+    //         System.out.print(str.charAt(i));
+    //     }
+    //     System.out.println();
+    // }
+    // public static void main(String args[]) {
+    //     String str = "Syed Maaz";
+    //     printStringelements(str);
+    //     printelementsbyindex(args);
+    // }
+
+
+    // //palindrome
+    // public static boolean isPalindrome(String str) {
+    //     int n = str.length();
+    //     for (int i=0; i<n/2; i++) {  //goes till half length on the string to check, if all the elements on the left side are same as right side
+    //         if (str.charAt(i) == str.charAt(n-1-i)) {
+    //             System.out.println("It is a palindrome");
+    //             return true;
+    //         }
+    //     }
+    //     System.out.println("NOT a palindrome");
+    //     return false;
+    // }
+    // public static void main(String args[]) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String str = sc.nextLine();
+    //     isPalindrome(str);
+    // }
+
+
+
 }
