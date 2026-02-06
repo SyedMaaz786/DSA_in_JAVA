b72f695c67eef75cf7beeef18acfb53cfb8d9c88
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-06 13:04:53 +0530

    String

--- c_Strings.java
+++ c_Strings.java
@@ -143,6 +143,65 @@ public class c_Strings {
     // }
 
 
+    // //String Builder
+    // public static void main(String args[]) {
+    //     StringBuilder sb = new StringBuilder();
+    //     for (char i='a'; i<='z'; i++) {
+    //         sb.append(i);
+    //     }
+    //     System.out.println(sb);
+    //     System.out.println(sb.length());
+    // }
+
+
+    // //convert to uppercase
+    // public static String toUpperCase(String str) {
+    //     StringBuilder sb = new StringBuilder();
+    //     char ch = Character.toUpperCase(str.charAt(0));
+    //     sb.append(ch);
+    //     for (int i=1; i<str.length(); i++) {
+    //         if (str.charAt(i) == ' ' && i<str.length()-1) {
+    //             sb.append(str.charAt(i));
+    //             i++;
+    //             sb.append(Character.toUpperCase(str.charAt(i)));
+    //         }
+    //         else {
+    //             sb.append(str.charAt(i));
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
+    // public static void main(String args[]) {
+    //     String str = "hi, i am maaz ";
+    //     System.out.println(toUpperCase(str));
+    // }
+
+
+    // //compress
+    // public static String compress(String str) {
+    //     StringBuilder sb = new StringBuilder();
+    //     String newstr = "";
+    //     sb.append(newstr);
+    //     for (int i=0; i<str.length(); i++) {
+    //         Integer count = 1;
+    //         while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+    //             count ++;
+    //             i++;
+    //         }
+    //         sb.append(str.charAt(i));
+    //         if (count > 1) {
+    //             sb.append(count.toString());
+    //         }
+    //     }
+    //     return sb.toString();
+    // }
+    // public static void main(String args[]) {
+    //     String str = "aaaabbbccd";
+    //     System.out.println(compress(str));
+    // }
+
+
+
 
 
 }
