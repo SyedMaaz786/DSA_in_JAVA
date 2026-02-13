0e0119f447f66a384641d82ae40db64d3f84a09a
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-13 12:49:01 +0530

    Recursion

--- f_Recursion01.java
+++ f_Recursion01.java
@@ -45,12 +45,24 @@ public class f_Recursion01 {
         int fn = fnm1 + fnm2;
         return fn;
     }
+    //Check if arr is sorted
+    public static boolean isSorted(int arr[], int i) {
+        if (i == arr.length-1) { //Base case
+            return true;
+        }
+        if (arr[i] > arr[i+1]) {
+            return false;
+        }
+        return isSorted(arr, i+1);
+    }
     public static void main(String args[]) {
-        int n = 10;
+        // int n = 10;
         // printDec(n);
         // printInc(n);
         // System.out.println(fact(n));
         // System.out.println(sum(n));
-        System.out.println(fib(n));
+        // System.out.println(fib(n));
+        int arr[] = {1,2,3,4};
+        System.out.println(isSorted(arr, 0));
     }
 }
