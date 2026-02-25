73470039e22298d20058fe3e3335e0d6dc5b5217
Syed Maaz <syedmaaz78622@gmail.com>
2026-02-25 10:53:36 +0530

    Backtracking

--- h_Backtracking.java
+++ h_Backtracking.java
@@ -66,13 +66,106 @@ public class h_Backtracking {
             findPermutations(newStr, ans + curr);
         }
     }
+
+
+    //Queens problem
+    public static boolean isSafe(char board[][], int row, int col) {
+        //vertical up
+        for (int i=row-1; i>=0; i--) {
+            if (board[i][col] == 'Q') {
+                return false;
+            }
+        }
+        //diagonal left
+        for (int i=row-1, j=col-1; i>=0 && j>=0; i--, j--) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        //diagonal right
+        for (int i=row-1, j=col+1; i>=0 && j<board.length; i--, j++) {
+            if (board[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+    //for all possible soln
+    public static void nQueens(char board[][], int row) {
+        //base case 
+        if (row == board.length) {
+            printBoard(board);
+            count ++;  //This is the optimisation to print the tot numb of ways to get the sol
+            return;
+        }
+        //recursion
+        //col loop to print Q
+        for (int col=0; col<board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 'Q';
+                nQueens(board, row+1); //fnx call
+                board[row][col] = '_'; //backtracking step
+            }    
+
+        }
+    }
+    
+    // //for one soln
+    // public static boolean nQueens(char board[][], int row) {
+    //     //base case 
+    //     if (row == board.length) {
+    //         printBoard(board);
+    //         count ++;  //This is the optimisation to print the tot numb of ways to get the sol
+    //         return true;
+    //     }
+    //     //recursion
+    //     //col loop to print Q
+    //     for (int col=0; col<board.length; col++) {
+    //         if (isSafe(board, row, col)) {
+    //             board[row][col] = 'Q';
+    //             if (nQueens(board, row+1)) { //fnx call
+    //                 return true;
+    //             }
+    //             board[row][col] = '_'; //backtracking step
+    //         }    
+
+    //     }
+    //     return false;
+    // }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("----Chess Board----");
+        for (int i=0; i<board.length; i++){
+            for (int j=0; j<board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    static int count = 0;
     public static void main(String args[]) {
         // int arr[] = new int[5];
         // changeArr(arr, 0, 1);
         // printArr(arr);
-        String str = "abc";
+        // String str = "abc";
         // printSubsets(str,new StringBuilder() , 0);
         // findSubsets(str, "", 0);
-        findPermutations(str, "");
+        // findPermutations(str, "");
+        int n = 4;
+        char board[][] = new char[n][n];
+        //Initializing value in board
+        for (int row=0; row<n; row++) {
+            for (int col=0; col<n; col++) {
+                board[row][col] = '_';
+            }
+        }
+        nQueens(board, 0);
+        System.out.println("Total numb of ways to solve nQueens problem is: " + count);
+        // if (nQueens(board, 0)) {
+        //     System.out.println("Solution is possible");
+        // }
+        // else {
+        //     System.out.println("Solution is not possible");
+        // }
     }
 }
