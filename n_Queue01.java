a33bdfc53454b5dd5b4ce72fec65d79e0384a1c9
Syed Maaz <syedmaaz78622@gmail.com>
2026-03-17 12:51:45 +0530

    Queue

--- n_Queue01.java
+++ n_Queue01.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class n_Queue01 {
 
     //Queue using Array
@@ -123,6 +124,217 @@ public class n_Queue01 {
         }
 
     }
+
+
+    //Queue using LinkedList
+    static class QueueC{
+        public class Node {
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public Node head = null;
+    public Node tail = null; 
+
+        public boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        //add fnx O(n)
+        public void add(int data){
+            //1st
+            Node newNode = new Node(data);
+            //3rd
+            if(head == null){  //add 1st ele
+                head = tail = newNode;
+                return;
+            }
+            //2st
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        //remove fnx O(n)
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            //1st
+            int front = head.data;
+            //3rd
+            if(tail == head){ //single element
+                tail = head = null;
+            }
+            else {
+                head = head.next;
+            }
+            //2nd
+            return front;
+        }
+
+        //peek fnx O(n)
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    
+    //Queue using two stacks (lecture if dought)
+    static class QueueD{
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public boolean isEmpty(){
+            return s1.isEmpty();
+        }
+        //add fnx O(n)
+        public void add(int data){
+            while(!s1.isEmpty()){  //until s1 gets empty
+                s2.push(s1.pop());  //s2 mai dalna s1 se nikalko
+            }
+            s1.push(data);  //elements assigned in s2 will be assigned back to s1 
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());  //vice versa uppar ka
+            }
+        }
+        //remove fnx O(1)
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return s1.pop();  //becoz our whole add fnx is designed in such a way that top of the s1 contains front
+        }
+        //peek fnx O(1)
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+
+
+    //stack using 2 queue's (lecture if dought)
+    static class QueueE {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public boolean isEmpty(){
+            return q1.isEmpty() && q2.isEmpty();
+        }
+        //push fnx O(1)
+        public void push(int data){
+            if(!q1.isEmpty()){  //at any time only one queue should have elements
+                q1.add(data);
+            }
+            else{
+                q2.add(data);
+            }
+        }
+        //pop fnx O(n)
+        public int pop(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int top = -1;
+            //case 1 (Byheart case 1 you can right case 2) byheart in the sense logic is same check for 2 !empty and empty 
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                    top = q1.remove();
+                    if(q1.isEmpty()){
+                        break;
+                    }
+                    q2.add(top);
+                }
+            }
+            //case 2
+            else{
+                while(!q2.isEmpty()){
+                    top = q2.remove();
+                    if(q2.isEmpty()){
+                        break;
+                    }
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+        //peek fnx (copy pop code and remove only if condition) O(n)
+        public int peek(){
+             if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int top = -1;
+            //case 1
+            if(!q1.isEmpty()){
+                while(!q1.isEmpty()){
+                    top = q1.remove();
+                    q2.add(top);
+                }
+            }
+            //case 2
+            else{
+                while(!q2.isEmpty()){
+                    top = q2.remove();
+                    q1.add(top);
+                }
+            }
+            return top;
+        }
+    }
+
+
+    //First Non Repeating character O(n) (lecture it is easy)
+    public static void printNonRepeating(String str){
+        int freq[] = new int[26];
+        Queue<Character> q = new LinkedList<>();
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;  //converts char to int and gives their freq and idx
+
+            while(!q.isEmpty() && freq[q.peek()-'a'] > 1){
+                q.remove();
+            }
+            if(q.isEmpty()){
+                System.out.print(-1 + " ");
+            }
+            else{
+                System.out.print(q.peek() + " ");
+            }
+        }
+        System.out.println();
+    }
+
+
+    //Interleave 2 halves of a queue O(n) (girls ki choti) GPT will explain very easy
+    public static void interLeave(Queue<Integer> q){
+        Queue<Integer> firstHalf = new LinkedList<>();
+        int size = q.size();
+
+        for(int i=0; i<size/2; i++){
+            firstHalf.add(q.remove());  //after the loop we will get firstHalf=1234 and q=5678
+        }
+        while(!firstHalf.isEmpty()){ //These 3 lines are the main logic dry run for 1234 you will get it
+            q.add(firstHalf.remove()); //firstHalf se remove krko q mai dal 
+            q.add(q.remove()); //phir q se remove krko phir q maich daal, ie where we get this interleave fashion, note remove will remove front and add will add at rear
+        }
+    }
+
     public static void main(String args[]){
         // QueueA q = new QueueA(5);
         // q.add(1);
@@ -134,20 +346,85 @@ public class n_Queue01 {
         // }
 
 
-        QueueB q = new QueueB(5);
+        // QueueB q = new QueueB(5);
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // // 1 2 3 
+        // System.out.println(q.remove());
+        // //1 
+        // q.add(4);
+        // System.out.println(q.remove());
+        // //2
+        // q.add(5);
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+
+
+        // QueueC q = new QueueC();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+
+        // //Queue using JCF(Java collection framework)
+        // Queue<Integer> q = new LinkedList<>();  //LinkedList because queue is a interface and we cannot create direct object using queue
+        // // Queue<Integer> q = new ArrayDeque<>();  //ArrayDeque can also be used 
+        // q.add(1);  
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+
+        // QueueD q = new QueueD();
+        // q.add(3);
+        // q.add(2);
+        // q.add(1);
+
+        // while(!q.isEmpty()){
+        //     System.out.println(q.peek());
+        //     q.remove();
+        // }
+
+
+        // Stack<Integer> s = new Stack<>();
+        // s.push(2);
+        // s.push(4);
+        // s.push(6);
+        // while(!s.isEmpty()){
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
+
+
+        // String str = "aabccxb";
+        // printNonRepeating(str);
+
+
+        Queue<Integer> q = new LinkedList<>();
         q.add(1);
         q.add(2);
         q.add(3);
-        // 1 2 3 
-        System.out.println(q.remove());
-        //1 
         q.add(4);
-        System.out.println(q.remove());
-        //2
         q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        interLeave(q);
         while(!q.isEmpty()){
-            System.out.println(q.peek());
-            q.remove();
+            System.out.print(q.remove() + " ");
         }
+        System.out.println();
     }
 }
