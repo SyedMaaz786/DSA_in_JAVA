803bc01a742c15802a8525909d2e3d7849607cbd
Syed Maaz <syedmaaz78622@gmail.com>
2026-03-27 17:08:58 +0530

    Binary Trees

---
+++ p_BinaryTrees01.java
@@ -0,0 +1,35 @@
+public class p_BinaryTrees01 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    static class BinaryTree{
+        static int idx = -1;
+        //1st
+        public Node buildTree(int nodes[]){
+            idx++;
+            //3rd
+            if(nodes[idx] == -1){
+                return null;
+            }
+            //2nd
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes); //recursive call
+            newNode.right = buildTree(nodes); //recursive call
+            return newNode;
+        }
+    }
+    public static void main (String args[]){
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
