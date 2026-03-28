bb64608196d5bf196f5be647b0b28d37e05c6b4c
Syed Maaz <syedmaaz78622@gmail.com>
2026-03-28 09:29:57 +0530

    Binary Trees

--- p_BinaryTrees01.java
+++ p_BinaryTrees01.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class p_BinaryTrees01 {
     static class Node{
         int data;
@@ -25,11 +28,99 @@ public class p_BinaryTrees01 {
             newNode.right = buildTree(nodes); //recursive call
             return newNode;
         }
+
+
+
+        //Preorder traversal - Recursive approach O(n)
+        public void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+        }
+
+
+        
+        //Inorder traversal - Recursive approach O(n)
+        public void inOrder(Node root){
+            if(root == null){
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+
+
+        //Postorder traversal - Recursive approach O(n)
+        public void postOrder(Node root){
+            if(root == null){
+                return;
+            }
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+
+
+        //Level order traversal - Iterative approach O(n)
+        public void levelOrder(Node root){
+            //7th
+            if(root == null){
+                return ;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null); //for printing nextline
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                //1st
+                if(currNode == null){
+                    System.out.println();
+                    //5th
+                    if(q.isEmpty()){
+                        break;
+                    }
+                    //6th
+                    else{
+                        q.add(null); //for printing nextline for other nodes
+                    }
+                }
+                //2nd
+                else{
+                    System.out.print(currNode.data + " ");
+                    //3rd
+                    if(currNode.left != null){
+                        q.add(currNode.left);
+                    }
+                    //4th
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
     }
     public static void main (String args[]){
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
+
+
+        // tree.preOrder(root);
+
+        // tree.inOrder(root);
+
+        // tree.postOrder(root);
+
+        tree.levelOrder(root);
+
+
     }
 }
