24730d74ddf8ed559d058b4bd28a492ad8d14c41
Syed Maaz <syedmaaz78622@gmail.com>
2026-04-05 09:48:14 +0530

    Binary Search Trees

--- q_BinarySearchTrees02.java
+++ q_BinarySearchTrees02.java
@@ -49,27 +49,89 @@ public class q_BinarySearchTrees02 {
         root = createBST(inorder, 0, inorder.size()-1);
         return root;  
     }
+    // Size of largest BST in BT (it's easy) notes for the steps and lecture for dryrun
+    static class Info{
+        boolean isBST;
+        int size;
+        int min;
+        int max;
+
+        public Info(boolean isBST, int size, int min, int max){
+            this.isBST = isBST;
+            this.size = size;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    public static int maxBST = 0;
+    public static Info largestBST(Node root){
+        if(root == null){
+            return new Info(true, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+        Info leftInfo = largestBST(root.left);
+        Info rightInfo = largestBST(root.right);
+        int size = leftInfo.size + rightInfo.size + 1;
+        int min = Math.min(root.data, Math.min(leftInfo.min, rightInfo.min));
+        int max = Math.max(root.data, Math.max(leftInfo.max, rightInfo.max));
+
+        // not valid BST
+        if(leftInfo.max >= root.data || rightInfo.min <= root.data){
+            return new Info(false, size, min, max);
+        }
+        // valid BST
+        if(leftInfo.isBST && rightInfo.isBST){
+            maxBST = Math.max(maxBST, size); //size is updated
+            return new Info(true, size, min, max);
+        }
+        else{
+            return new Info(false, size, min, max);
+        }
+    }
     public static void main(String args[]){
-        /*      8
-              /   \
-             6     10      normal BST
-            /       \
-           5         11 
-          /           \
-         3             12  */
-        Node root = new Node(8);
-        root.left = new Node(6);        
+        // /*      8
+        //       /   \
+        //      6     10      normal BST
+        //     /       \
+        //    5         11 
+        //   /           \
+        //  3             12  */
+        // Node root = new Node(8);
+        // root.left = new Node(6);        
+        // root.left.left = new Node(5);
+        // root.left.left.left = new Node(3);
+        // root.right = new Node(10);
+        // root.right.right = new Node(11);
+        // root.right.right.right = new Node(12);
+        // /*   8
+        //    /   \
+        //   5     11     Balanced BST
+        //  / \   /  \
+        // 3   6 10  12  */
+        // root = balanceBST(root);
+        // preOrder(root);
+
+
+
+
+        
+        /*      50
+              /    \
+            30      60
+           /  \    /  \
+          5   20  45   70
+                         / \
+                        65  80  */
+        Node root = new Node(50);
+        root.left = new Node(30);
         root.left.left = new Node(5);
-        root.left.left.left = new Node(3);
-        root.right = new Node(10);
-        root.right.right = new Node(11);
-        root.right.right.right = new Node(12);
-        /*   8
-           /   \
-          5     11     Balanced BST
-         / \   /  \
-        3   6 10  12  */
-        root = balanceBST(root);
-        preOrder(root);
+        root.left.right = new Node(20);
+
+        root.right = new Node(60);
+        root.right.left = new Node(45);
+        root.right.right = new Node(70);
+        root.right.right.left = new Node(65);
+        root.right.right.right = new Node(80);
+        Info info = largestBST(root);
+        System.out.println("Largest BST size is: " + maxBST);
     }
 }
