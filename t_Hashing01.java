b1d20bbf845746be4e1d2a5b9ed22c62aa7e105c
Syed Maaz <syedmaaz78622@gmail.com>
2026-04-07 11:24:51 +0530

    Hashing

---
+++ t_Hashing01.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class t_Hashing01 {
+    public static void main(String srgs[]){
+        //Create 
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        //Insert O(1)
+        hm.put("India", 100);
+        hm.put("China", 200);
+        hm.put("Bhutan", 50);
+        System.out.println(hm);
+
+        //Get O(1)
+        System.out.println(hm.get("India"));
+
+        //ContainsKey O(1)
+        System.out.println(hm.containsKey("India"));
+
+        //Remove O(1)
+        System.out.println(hm.remove("India", 100));
+        System.out.println(hm);
+
+        //Size
+        System.out.println(hm.size());
+
+        //isEmpty
+        System.out.println(hm.isEmpty());
+
+        //clear
+        hm.clear();
+        System.out.println(hm);
+    }
+}
