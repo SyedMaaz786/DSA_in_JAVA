65c249abb1a4174e20b587788306d909f43187af
Syed Maaz <syedmaaz78622@gmail.com>
2026-04-16 13:46:32 +0530

    Graph

--- v_Graph04.java
+++ v_Graph04.java
@@ -17,18 +17,32 @@ public class v_Graph04 {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1, 2));
+        // graph[0].add(new Edge(0, 1, 2)); // dijkstra
+        // graph[0].add(new Edge(0, 2, 4));
+
+        // graph[1].add(new Edge(1, 3, 7));
+        // graph[1].add(new Edge(1, 2, 1));
+
+        // graph[2].add(new Edge(2, 4, 3));
+
+        // graph[3].add(new Edge(3, 5, 1));
+
+        // graph[4].add(new Edge(4, 3, 2));
+        // graph[4].add(new Edge(4, 5, 5));
+
+
+
+
+        graph[0].add(new Edge(0, 1, 2)); // Bellman ford
         graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 3, 7));
-        graph[1].add(new Edge(1, 2, 1));
+        graph[1].add(new Edge(1, 2, -4));
 
-        graph[2].add(new Edge(2, 4, 3));
+        graph[2].add(new Edge(2, 3, 2));
 
-        graph[3].add(new Edge(3, 5, 1));
+        graph[3].add(new Edge(3, 4, 4));
 
-        graph[4].add(new Edge(4, 3, 2));
-        graph[4].add(new Edge(4, 5, 5));
+        graph[4].add(new Edge(4, 1, -1));
     }
     // Dijkstra's Algorithm (to find shortest distance from src to dest) O(V+ElogV) (lecture for understanding and dryrun)
     static class Pair implements Comparable<Pair>{
@@ -65,6 +79,7 @@ public class v_Graph04 {
                 int v = e.dest;
                 int wt = e.wt;
 
+                //relaxation
                 if(dist[u]+wt < dist[v]){ //formula remember this notes mai bi likhko hu
                     dist[v] = dist[u]+wt;
                     pq.add(new Pair(v, dist[v])); //pq mai add krna node bi path
@@ -77,12 +92,46 @@ public class v_Graph04 {
         }
         System.out.println();
     }
+    //BellmanFord Algorith (-ve weights) O(V*E) (it's easy check lecture for understanding and dryrun)
+    public static void bellmanFord(ArrayList<Edge> graph[], int src){
+        int dist[] = new int[graph.length];
+        for(int i=0; i<dist.length; i++){
+            if(i != src){
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
+        int V = graph.length;  //defined V for simplicity or else graph.length-1 would also work same
+        for(int i=0; i<V-1; i++){ //O(V)
+            //edges O(E)
+            for(int j=0; j<graph.length; j++){ //loop -> vertices
+                for(int k=0; k<graph[j].size(); k++){ //loop -> edges
+                    Edge e = graph[j].get(k);
+                    int u = e.src;
+                    int v = e.dest;
+                    int wt = e.wt;
+                    if(dist[u] != Integer.MAX_VALUE && dist[u]+wt < dist[v]){ //relaxation with modified
+                        dist[v] = dist[u]+wt;
+                    } 
+                }
+            }
+        }
+        //print
+        for(int i=0; i<dist.length; i++){
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
-        int V = 6;
+        // int V = 6;
+        // ArrayList<Edge> graph[] = new ArrayList[V];
+        // createGraph(graph);
+        // dijkstra(graph, 0);
+
+
+        int V = 5;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        int src = 0;
-        dijkstra(graph, src);
+        bellmanFord(graph, 0);
 
     }
 }
