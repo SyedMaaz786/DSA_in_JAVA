1f40f6b75c710982d105c6d3415ce00aae9d5dd2
Syed Maaz <syedmaaz78622@gmail.com>
2026-04-16 09:25:33 +0530

    Graph

--- v_Graph03.java
+++ v_Graph03.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class v_Graph03 {
@@ -43,32 +45,78 @@ public class v_Graph03 {
         stack[curr] = false;
         return false;
     }
-    //Topological Sorting (using DFS) O(L+E) (it's easy)
-    //2nd
-    public static void topSort(ArrayList<Edge> graph[]){
-        boolean visited[] = new boolean[graph.length];
-        Stack<Integer> s = new Stack<>();
+    // //Topological Sorting (using DFS) O(L+E) (it's easy)
+    // //2nd
+    // public static void topSort(ArrayList<Edge> graph[]){
+    //     boolean visited[] = new boolean[graph.length];
+    //     Stack<Integer> s = new Stack<>();
 
+    //     for(int i=0; i<graph.length; i++){
+    //         if(!visited[i]){
+    //             topSortUtil(graph, i, visited, s);
+    //         }
+    //     }
+    //     while(!s.isEmpty()){
+    //         System.out.print(s.pop() + " ");
+    //     }
+    // }
+    // //1st
+    // public static void topSortUtil(ArrayList<Edge> graph[], int curr, boolean visited[], Stack<Integer> s){
+    //     visited[curr] = true;
+
+    //     for(int i=0; i<graph[curr].size(); i++){
+    //         Edge e = graph[curr].get(i);
+    //         if(!visited[e.dest]){
+    //             topSortUtil(graph, e.dest, visited, s);
+    //         }
+    //     }
+    //     s.push(curr);
+    // }
+    //Topological Sorting (using BFS) O(L+E) (lecture for understanding and dryrun)
+    //2nd
+    public static void calcIndeg(ArrayList<Edge> graph[], int indeg[]){
         for(int i=0; i<graph.length; i++){
-            if(!visited[i]){
-                topSortUtil(graph, i, visited, s);
+            for(int j=0; j<graph[i].size(); j++){
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
             }
-        }
-        while(!s.isEmpty()){
-            System.out.print(s.pop() + " ");
         }
     }
     //1st
-    public static void topSortUtil(ArrayList<Edge> graph[], int curr, boolean visited[], Stack<Integer> s){
-        visited[curr] = true;
+    public static void topSort(ArrayList<Edge> graph[]){
+        int indeg[] = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0; i<graph[curr].size(); i++){
-            Edge e = graph[curr].get(i);
-            if(!visited[e.dest]){
-                topSortUtil(graph, e.dest, visited, s);
+        for(int i=0; i<indeg.length; i++){
+            if(indeg[i] == 0){
+                q.add(i);
             }
         }
-        s.push(curr);
+        //3rd
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            System.out.print(curr + " "); //topological sort print
+            
+            for(int i=0; i<graph[curr].size(); i++){
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest] == 0){
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+    //All paths from source to destination TC-exponential O(2^V × V) (it's easy lecture for understanding and dryrun)
+    public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path){
+        if(src == dest){
+            System.out.print(path + dest);
+            return;
+        }
+        for(int i=0; i<graph[src].size(); i++){
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path + src);
+        }
     }
     // static void createGraph(ArrayList<Edge> graph[]){
     //     for(int i=0; i<graph.length; i++){
@@ -101,6 +149,9 @@ public class v_Graph03 {
         // System.out.println(isCycle(graph));
 
 
-        topSort(graph);
+        // topSort(graph);
+
+        int src = 0, dest = 3;
+        printAllPath(graph, src, dest, " ");
     }
 }
