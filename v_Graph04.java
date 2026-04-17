2366543701145a689f7a8c0b0c6f51434400dcd2
Syed Maaz <syedmaaz78622@gmail.com>
2026-04-17 09:53:43 +0530

    Graph

--- v_Graph04.java
+++ v_Graph04.java
@@ -33,16 +33,32 @@ public class v_Graph04 {
 
 
 
-        graph[0].add(new Edge(0, 1, 2)); // Bellman ford
-        graph[0].add(new Edge(0, 2, 4));
+        // graph[0].add(new Edge(0, 1, 2)); // Bellman ford
+        // graph[0].add(new Edge(0, 2, 4));
 
-        graph[1].add(new Edge(1, 2, -4));
+        // graph[1].add(new Edge(1, 2, -4));
 
-        graph[2].add(new Edge(2, 3, 2));
+        // graph[2].add(new Edge(2, 3, 2));
 
-        graph[3].add(new Edge(3, 4, 4));
+        // graph[3].add(new Edge(3, 4, 4));
 
-        graph[4].add(new Edge(4, 1, -1));
+        // graph[4].add(new Edge(4, 1, -1));
+
+
+
+
+        graph[0].add(new Edge(0, 1, 10));  // Prim's 
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
+
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
+
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 50));
+
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
     }
     // Dijkstra's Algorithm (to find shortest distance from src to dest) O(V+ElogV) (lecture for understanding and dryrun)
     static class Pair implements Comparable<Pair>{
@@ -121,6 +137,41 @@ public class v_Graph04 {
         }
         System.out.println();
     }
+    //Prim's Algorithm (MST) (it's easy cheack lecture for understanding and dryrun)
+    static class Pair2 implements Comparable<Pair2>{
+        int vertex;
+        int weight;
+
+        public Pair2(int vertex, int weight){
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+        @Override 
+        public int compareTo(Pair2 p2){
+            return this.weight - p2.weight; //ascending
+        }
+    }
+    public static void prims(ArrayList<Edge> graph[]){
+        boolean visited[] = new boolean[graph.length];
+        PriorityQueue<Pair2> pq = new PriorityQueue<>();
+        pq.add(new Pair2(0, 0));
+        int finalWeight = 0; //total weight
+
+        while(!pq.isEmpty()){
+            Pair2 curr = pq.remove();
+            if(!visited[curr.vertex]){
+                visited[curr.vertex] = true;
+                finalWeight += curr.weight;
+
+                for(int i=0; i<graph[curr.vertex].size(); i++){
+                    Edge e = graph[curr.vertex].get(i);
+                    pq.add(new Pair2(e.dest, e.wt));
+                }
+            }
+
+        }
+        System.out.println("Total(min) weight of MST: " + finalWeight);
+    }
     public static void main(String args[]){
         // int V = 6;
         // ArrayList<Edge> graph[] = new ArrayList[V];
@@ -128,10 +179,17 @@ public class v_Graph04 {
         // dijkstra(graph, 0);
 
 
-        int V = 5;
+        // int V = 5;
+        // ArrayList<Edge> graph[] = new ArrayList[V];
+        // createGraph(graph);
+        // bellmanFord(graph, 0);
+
+
+        int V = 4;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        bellmanFord(graph, 0);
+        prims(graph);
+
 
     }
 }
