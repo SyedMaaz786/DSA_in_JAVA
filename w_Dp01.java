2f12fdff33f45cc1d7973db4322902c1cf94219d
Syed Maaz <syedmaaz78622@gmail.com>
2026-05-02 16:07:38 +0530

    DP

--- w_Dp01.java
+++ w_Dp01.java
@@ -208,6 +208,7 @@ public class w_Dp01 {
         return dp[n][rodLength];
     }
     // Longest common subsequence
+    //recursion
     public static int lcs(String str1, String str2, int n, int m){
         if(n == 0 || m == 0){
             return 0;
@@ -220,6 +221,39 @@ public class w_Dp01 {
             int ans2 = lcs(str1, str2, n, m-1);
             return Math.max(ans1, ans2);
         }
+    }
+    //memoization
+    public static int lcs2(String str1, String str2, int dp[][], int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(dp[n][m] != -1){
+            return dp[n][m];
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
+            return dp[n][m] = lcs2(str1, str2, dp, n-1, m-1) + 1;
+        }
+        else{//diff
+            int ans1 = lcs2(str1, str2, dp, n-1, m);
+            int ans2 = lcs2(str1, str2, dp, n, m-1);
+            return dp[n][m] = Math.max(ans1, ans2);
+        }
+    }
+    //tabulation
+    public static int lcs3(String str1, String str2, int dp[][], int n, int m){
+        for(int i=1; i<=str1.length(); i++){
+            for(int j=1; j<=str2.length(); j++){
+                if(str1.charAt(i-1) == str2.charAt(j-1)){ //same
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }
+                else{ //diff
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
     }
     public static void main(String args[]){
         // int n = 5;
@@ -275,8 +309,30 @@ public class w_Dp01 {
         // System.out.println(rodCutting(length, price, rodLength, dp, length.length));
 
 
+        // String str1 = "abcdge";
+        // String str2 = "abedg";
+        // System.out.println(lcs(str1, str2, str1.length(), str2.length()));
+
+
+        // String str1 = "abcdge";
+        // String str2 = "abedg";
+        // int dp[][] = new int[str1.length()+1][str2.length()+1];
+        // for(int i=0; i<dp.length; i++){ //init 0th col and row with 0
+        //     for(int j=0; j<dp[0].length; j++){
+        //         dp[i][j] = -1;
+        //     }
+        // }
+        // System.out.println(lcs2(str1, str2, dp, str1.length(), str2.length()));
+
+
         String str1 = "abcdge";
         String str2 = "abedg";
-        System.out.println(lcs(str1, str2, str1.length(), str2.length()));
+        int dp[][] = new int[str1.length()+1][str2.length()+1];
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++){
+                dp[i][j] = 0;
+            }
+        }
+        System.out.println(lcs3(str1, str2, dp, str1.length(), str2.length()));
     }
 }
