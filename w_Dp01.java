c823a661a654d8463a31b470f41cde660c991a6d
Syed Maaz <syedmaaz78622@gmail.com>
2026-05-02 13:42:34 +0530

    DP

--- w_Dp01.java
+++ w_Dp01.java
@@ -157,6 +157,70 @@ public class w_Dp01 {
         }
         return dp[n][C];
     }
+    //coin change
+    public static int coinChange(int coins[], int sum, int dp[][], int n){
+        for(int i=0; i<dp.length; i++){ //init 0th col with 1
+            dp[i][0] = 1;
+        }
+        for(int j=0; j<dp[0].length; j++){ 
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<coins.length+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(coins[i-1] <= j){ //valid
+                    //include
+                    int ans1 = dp[i][j-coins[i-1]];
+                    //exclude
+                    int ans2 = dp[i-1][j];
+                    dp[i][j] = ans1 + ans2; //sum if total ways kind of questions (compare if max, min or anyother type of questions)
+                }
+                else{ //invalid
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+    //rod cutting 
+    public static int rodCutting(int length[], int price[], int rodLength, int dp[][], int n){
+        for(int i=0; i<dp.length; i++){ //init 0th col with 0
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<dp[0].length; j++){ //init 0th row with 0
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<length.length+1; i++){
+            for(int j=1; j<rodLength+1; j++){
+                if(length[i-1] <= j){ //valid
+                    //include
+                    int ans1 = price[i-1] + dp[i][j-length[i-1]];
+                    //exclude
+                    int ans2 = dp[i-1][j];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][rodLength];
+    }
+    // Longest common subsequence
+    public static int lcs(String str1, String str2, int n, int m){
+        if(n == 0 || m == 0){
+            return 0;
+        }
+        if(str1.charAt(n-1) == str2.charAt(m-1)){ //same
+            return lcs(str1, str2, n-1, m-1) + 1;
+        }
+        else{ //diff
+            int ans1 = lcs(str1, str2, n-1, m);
+            int ans2 = lcs(str1, str2, n, m-1);
+            return Math.max(ans1, ans2);
+        }
+    }
     public static void main(String args[]){
         // int n = 5;
         // int dp[] = new int[n+1]; // 0, 0, 0, 0, 0, 0 (imp n+1 because we will store 0 also in fib, so it goes like 0,1,1,2,3 and so on)
@@ -191,10 +255,28 @@ public class w_Dp01 {
 
 
         
-        int val[] = {15, 14, 10, 45, 30};
-        int wt[] = {2, 5, 1, 3, 4};
-        int C = 7;
-        int dp[][] = new int[val.length+1][C+1];
-        System.out.println(unboundedKnapsack(val, wt, C, val.length, dp));
+        // int val[] = {15, 14, 10, 45, 30};
+        // int wt[] = {2, 5, 1, 3, 4};
+        // int C = 7;
+        // int dp[][] = new int[val.length+1][C+1];
+        // System.out.println(unboundedKnapsack(val, wt, C, val.length, dp));
+
+
+        // int coins[] = {2, 5, 3, 6};
+        // int sum = 10;
+        // int dp[][] = new int[coins.length+1][sum+1];
+        // System.out.println(coinChange(coins, sum, dp, coins.length));
+
+
+        // int length[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        // int price[] = {1, 5, 8, 9, 10, 17, 17, 20};
+        // int rodLength = 8;
+        // int dp[][] = new int[length.length+1][rodLength+1];
+        // System.out.println(rodCutting(length, price, rodLength, dp, length.length));
+
+
+        String str1 = "abcdge";
+        String str2 = "abedg";
+        System.out.println(lcs(str1, str2, str1.length(), str2.length()));
     }
 }
