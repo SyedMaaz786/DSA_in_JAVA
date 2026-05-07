edcfc96a7a4b09b4ea9e6b4dd224c67133d5e404
Syed Maaz <syedmaaz78622@gmail.com>
2026-05-07 15:26:39 +0530

    Segment Tree

--- x_SegmentTrees01.java
+++ x_SegmentTrees01.java
@@ -3,19 +3,19 @@ public class x_SegmentTrees01 {
     public static void init(int n){
         tree = new int[4*n];
     }
-    public static int buildST(int arr[], int sti, int start, int end){
-        if(start == end){
-            tree[sti] = arr[start];
-            return arr[start];
+    public static int buildST(int arr[], int sti, int si, int sj){ //si = starting idx, sj = ending idx
+        if(si == sj){
+            tree[sti] = arr[si];
+            return arr[si];
         }
-        int mid = (start + end) / 2;
-        buildST(arr, 2*sti+1, start, mid);
-        buildST(arr, 2*sti+2, mid+1, end);
-        tree[sti] = tree[2*sti+1] + tree[2*sti+2];
+        int mid = (si + sj) / 2;
+        buildST(arr, 2*sti+1, si, mid);
+        buildST(arr, 2*sti+2, mid+1, sj);
+        tree[sti] = tree[2*sti+1] + tree[2*sti+2]; //logic for sum
         return tree[sti];
     }
     //query O(logn)
-    public static int getSumUtil(int sti, int si, int sj, int qi, int qj){
+    public static int getSumUtil(int sti, int si, int sj, int qi, int qj){ //si = starting idx, sj = ending idx
         if(qi >= sj || qj <= si){ //non overlapping 
             return 0;
         }
@@ -33,6 +33,24 @@ public class x_SegmentTrees01 {
         int n = arr.length;
         return getSumUtil(0, 0, n-1, qi, qj);
     }
+    //update O(logn)
+    public static void updateUtil(int sti, int si, int sj, int ui, int diff){ //si = starting idx, sj = ending idx, ui = update idx
+        if(ui < si || ui > sj){ //non overlapping
+            return;
+        }
+        tree[sti] += diff; //this is else complete overlapping meaning valid condition so we add the diff
+        if(si != sj){ //non leaf
+            int mid = (si + sj) / 2; //partial overlapping
+            updateUtil(2*sti+1, si, mid, ui, diff);
+            updateUtil(2*sti+2, mid+1, sj, ui, diff);
+        }
+    }
+    public static void update(int arr[], int idx, int newVal){
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+        updateUtil(0, 0, n-1, idx, diff);
+    }
     public static void main(String args[]){
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -44,6 +62,8 @@ public class x_SegmentTrees01 {
         }
         System.out.println();
         
+        System.out.println(getSum(arr, 2, 5));
+        update(arr, 2, 2);
         System.out.println(getSum(arr, 2, 5));
     }
 }
---
+++ x_SegmentTrees02.java
@@ -0,0 +1,68 @@
+public class x_SegmentTrees02 {
+    static int tree[];
+    public static void init(int n){
+        tree = new int[4*n];
+    }
+    public static int buildST(int arr[], int sti, int si, int sj){
+        if(si == sj){
+            tree[sti] = arr[si];
+            return tree[sti];
+        }
+        int mid = (si+sj)/2;
+        buildST(arr, 2*sti+1, si, mid);
+        buildST(arr, 2*sti+2, mid+1, sj);
+        tree[sti] = Math.max(tree[2*sti+1], tree[2*sti+2]); // logic for max 
+        return tree[sti];
+    }
+    //query
+    public static int getMaxUtil(int sti, int si, int sj, int qi, int qj){
+        if(sj < qi || si > qj){ //non overlap
+            return Integer.MIN_VALUE;
+        }
+        else if(si >= qi && sj <= qj){
+            return tree[sti];
+        }
+        else{
+            int mid = (si+sj)/2;
+            int left = getMaxUtil(2*sti+1, si, mid, qi, qj);
+            int right = getMaxUtil(2*sti+2, mid+1, sj, qi, qj);
+            return Math.max(left, right);
+        }
+    }
+    public static int getMax(int arr[], int qi, int qj){
+        int n = arr.length;
+        return getMaxUtil(0, 0, n-1, qi, qj);
+    }
+    //update
+    public static void updateUtil(int sti, int si, int sj, int ui, int newVal){
+        if(ui < si || ui > sj){ //non overlapping
+            return;
+        }
+        tree[sti] = Math.max(tree[sti], newVal);
+        if(si != sj){
+            int mid = (si+sj)/2;
+            updateUtil(2*sti+1, si, mid, ui, newVal);
+            updateUtil(2*sti+2, mid+1, sj, ui, newVal);
+        }
+    }
+    public static void update(int arr[], int idx, int newVal){
+        int n = arr.length;
+        arr[idx] = newVal;
+        updateUtil(0, 0, n-1, idx, newVal);
+    }
+    public static void main(String args[]){
+        int arr[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
+        int n = arr.length;
+        init(n);
+        buildST(arr, 0, 0, n-1);
+
+        for(int i=0; i<tree.length; i++){
+            System.out.print(tree[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println(getMax(arr, 0, 8));
+        update(arr, 2, 20);
+        System.out.println(getMax(arr, 0, 8));
+    }
+}
