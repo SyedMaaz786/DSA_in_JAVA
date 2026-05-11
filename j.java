d9dff5e8c19d24d489e3179ea5767b299920a1d5
Syed Maaz <syedmaaz78622@gmail.com>
2026-05-11 16:46:16 +0530

    revision

--- j.java
+++ j.java
@@ -1,16 +1,20 @@
 import java.util.*;
 
 public class j {
-    public static void zeroOne(int rows, int cols){
-        for(int i=1; i<=rows; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(((i+j) % 2 == 0) ? '1' : '0');
+    public static void pairs(int numbers[]){
+        int tp = 0;
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                System.out.println(numbers[i] + "," + numbers[j]);
+                tp++;
             }
             System.out.println();
         }
+        System.out.println(tp);
+        
     }
     public static void main(String args[]){
-        zeroOne(2,2);
+        int numbers[] = {2, 3, 4, 5, 6, 7, 8, 9};
+        pairs(numbers);
     }
-
 }
