e594d7e20275fc6748b7d04856e53c988ea55772
Maaz <syedmaaz78622@gmail.com>
2026-06-06 14:27:19 +0530

    revision

--- t_Hashing03.java
+++ t_Hashing03.java
@@ -1,19 +1,19 @@
 import java.util.*;
 
 public class t_Hashing03 {
-    //Find itinerary for tickets
-    public static String startingPoint(HashMap<String, String> map1){ //O(n) lecture if dought stare it it's easy
-        HashMap<String, String> map2 = new HashMap<>(); //created a reversehashmap (revhm) 
-        for(String key: map1.keySet()){ 
-            map2.put(map1.get(key), key); //here we puts value as key and key as value (from->to) becomes (to->from)
-        }
-        for(String key: map1.keySet()){
-            if(!map2.containsKey(key)){
-                return key; //gives starting point
-            }
-        }
-        return null; 
-    }
+    // //Find itinerary for tickets
+    // public static String startingPoint(HashMap<String, String> map1){ //O(n) lecture if dought stare it it's easy
+    //     HashMap<String, String> map2 = new HashMap<>(); //created a reversehashmap (revhm) 
+    //     for(String key: map1.keySet()){ 
+    //         map2.put(map1.get(key), key); //here we puts value as key and key as value (from->to) becomes (to->from)
+    //     }
+    //     for(String key: map1.keySet()){
+    //         if(!map2.containsKey(key)){
+    //             return key; //gives starting point
+    //         }
+    //     }
+    //     return null; 
+    // }
     public static void main(String args[]){
         //HashSet (Unordered)
         HashSet<String> hs = new HashSet<>();
@@ -23,98 +23,121 @@ public class t_Hashing03 {
         hs.add("Bengaluru");
         System.out.println(hs);
 
-        //Iterate in set using Iterator
-        Iterator i = hs.iterator();
-        while(i.hasNext()){
-            System.out.println(i.next());
-        }
+    //     //Iterate in set using Iterator
+    //     Iterator i = hs.iterator();
+    //     while(i.hasNext()){
+    //         System.out.println(i.next());
+    //     }
 
-        //Iterate using Advance loop
-        for(String s : hs){
-            System.out.println(s);
-        }
+    //     //Iterate using Advance loop
+    //     for(String s : hs){
+    //         System.out.println(s);
+    //     }
 
-        //LinkedHashSet (Ordered)
-        LinkedHashSet<String> lhs = new LinkedHashSet<>();
-        lhs.add("Kolkata");
-        lhs.add("Lucknow");
-        lhs.add("Noida");
-        System.out.println(lhs);
+    //     //LinkedHashSet (Ordered)
+    //     LinkedHashSet<String> lhs = new LinkedHashSet<>();
+    //     lhs.add("Kolkata");
+    //     lhs.add("Lucknow");
+    //     lhs.add("Noida");
+    //     System.out.println(lhs);
 
-        //TreeSet (Sorted in ascending order)
-        TreeSet<Integer> ts = new TreeSet<>();
-        ts.add(500);
-        ts.add(200);
-        ts.add(50);
-        System.out.println(ts);
-
-
-        //Count Distinct Elements
-        int num[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i=0; i<num.length; i++){
-            hs.add(num[i]);
-        }
-        System.out.println("Answer: " + hs.size());
+    //     //TreeSet (Sorted in ascending order)
+    //     TreeSet<Integer> ts = new TreeSet<>();
+    //     ts.add(500);
+    //     ts.add(200);
+    //     ts.add(50);
+    //     System.out.println(ts);
 
 
-        //Union and Intersection (it's very easy)
-        int arr1[] = {7, 3, 9};
-        int arr2[] = {6, 3, 9, 2, 9, 4};
-        HashSet<Integer> hs = new HashSet<>();
-        //Union
-        for(int i=0; i<arr1.length; i++){
-            hs.add(arr1[i]);
-        }
-        for(int i=0; i<arr2.length; i++){
-            hs.add(arr2[i]);
-        }
-        System.out.println("Union size: " + hs.size());
-        System.out.println("Union elements: " + hs);
-        //Intersection
-        hs.clear(); //clearing it so that we can start intersection again on the same hs
-        for(int i=0; i<arr1.length; i++){
-            hs.add(arr1[i]);
-        }
-        int count = 0;
-        for(int i=0; i<arr2.length; i++){
-            if(hs.contains(arr2[i])){
-                count++;
-                System.out.println("Intersection element: " + arr2[i]);
-                hs.remove(arr2[i]);
-            }
-        }
-        System.out.println("Intersection size: " + count);
+    //     //Count Distinct Elements
+    //     int num[] = {4, 3, 2, 5, 6, 7, 3, 4, 2, 1};
+    //     HashSet<Integer> hs = new HashSet<>();
+    //     for(int i=0; i<num.length; i++){
+    //         hs.add(num[i]);
+    //     }
+    //     System.out.println("Answer: " + hs.size());
 
 
-        HashMap<String, String> map1 = new HashMap<>();
-        map1.put("Chennai", "Bengaluru");
-        map1.put("Mumbai", "Delhi");
-        map1.put("Goa", "Chennai");
-        map1.put("Delhi", "Goa");
-        String start = startingPoint(map1); //this gives starting point ie mumbai by calling the starting fnx we created above
-        System.out.print(start);
-        for(int i=0; i<map1.size(); i++){
-            System.out.print("->" + map1.get(start)); //this is value from map1
-            start = map1.get(start); //start becomes value now for next iteration
-        }
+    //     //Union and Intersection (it's very easy)
+    //     int arr1[] = {7, 3, 9};
+    //     int arr2[] = {6, 3, 9, 2, 9, 4};
+    //     HashSet<Integer> hs = new HashSet<>();
+    //     //Union
+    //     for(int i=0; i<arr1.length; i++){
+    //         hs.add(arr1[i]);
+    //     }
+    //     for(int i=0; i<arr2.length; i++){
+    //         hs.add(arr2[i]);
+    //     }
+    //     System.out.println("Union size: " + hs.size());
+    //     System.out.println("Union elements: " + hs);
+    //     //Intersection
+    //     hs.clear(); //clearing it so that we can start intersection again on the same hs
+    //     for(int i=0; i<arr1.length; i++){
+    //         hs.add(arr1[i]);
+    //     }
+    //     int count = 0;
+    //     for(int i=0; i<arr2.length; i++){
+    //         if(hs.contains(arr2[i])){
+    //             count++;
+    //             System.out.println("Intersection element: " + arr2[i]);
+    //             hs.remove(arr2[i]);
+    //         }
+    //     }
+    //     System.out.println("Intersection size: " + count);
 
 
-        //Largest Subarray with Sum 0 O(n) - (lecture video must watch for dryrun and understanding)
-        int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
-        HashMap<Integer, Integer> hm = new HashMap<>(); //store sum, idx 
-        int sum = 0;
-        int len = 0;
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
-            if(hm.containsKey(sum)){
-                len = Math.max(len, i-hm.get(sum)); //hm.get(sum) dry run and see it will give the value from hm ie the idx value 
-            }
-            else{
-                hm.put(sum, i);
-            }
-        }
-        System.out.println("Largest Subarray with Sum 0: " + len);
+    //     HashMap<String, String> map1 = new HashMap<>();
+    //     map1.put("Chennai", "Bengaluru");
+    //     map1.put("Mumbai", "Delhi");
+    //     map1.put("Goa", "Chennai");
+    //     map1.put("Delhi", "Goa");
+    //     String start = startingPoint(map1); //this gives starting point ie mumbai by calling the starting fnx we created above
+    //     System.out.print(start);
+    //     for(int i=0; i<map1.size(); i++){
+    //         System.out.print("->" + map1.get(start)); //this is value from map1
+    //         start = map1.get(start); //start becomes value now for next iteration
+    //     }
+
+
+    //     //Largest Subarray with Sum 0 O(n) - (Their is an imp presum property which is used in this problem, check new notes for that)
+    //     int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    //     HashMap<Integer, Integer> hm = new HashMap<>(); //store sum, idx 
+    //     int sum = 0;
+    //     int len = 0;
+    //     for(int i=0; i<arr.length; i++){
+    //         sum += arr[i];
+    //         if(hm.containsKey(sum)){
+    //             len = Math.max(len, i-hm.get(sum)); //len = j-i check lecture imp 
+    //         }
+    //         else{
+    //             hm.put(sum, i);
+    //         }
+    //     }
+    //     System.out.println("Largest Subarray with Sum 0: " + len);
+
+
+        // //Subarr Sum Equal to k (very imp, explain notes math equations very intuitive and dryrun must check lecture)
+        // int arr[] = {10, 2, -2, -20, 10};
+        // int k = -10;
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // map.put(0, 1); //check why this in lecture
+
+        // int sum = 0;
+        // int ans = 0;
+
+        // for(int j=0; j<arr.length; j++){
+        //     sum+= arr[i];
+        //     if(map.containsKey(sum-k)){
+        //         ans+= map.get(sum-k);
+        //     }
+        //     map.put(sum, map.getOrDefault(sum, 0) + 1);
+        // }
+
+
+        //IMPORTANT their is an imp problem called LRU cahched on LC check it.
+
 
     }
 }
